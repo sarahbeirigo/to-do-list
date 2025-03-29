cf82659abc7a989087495bd5c0ffb3a3201014f8
Sarah Beirigo <sarahcbeirigo@gmail.com>
2025-03-28 22:17:08 -0300

    feat: criar e mostrar tarefa na tela

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-
+document.addEventListener("DOMContentLoaded", carregarTarefas);
 
 function salvarTarefaLocalStorage() {
     const input = document.getElementById("tarefa-input");
@@ -23,5 +23,77 @@ function salvarTarefaLocalStorage() {
     // Limpa o input
     input.value = "";
     
+    // Atualiza a lista na tela
+    carregarTarefas();
 }
+
+function carregarTarefas() {
+    const lista = document.getElementById("lista-tarefas");
+    lista.innerHTML = ""; // Limpa a lista antes de recriá-la
+
+
+    let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+    tarefas.forEach((tarefa, index) => {
+        const li = document.createElement("li");
+        li.classList.add("flex", "justify-between", "align-center", "mb-6", "items-center");
+
+        const container = document.createElement("div");
+        container.classList.add("flex", "align-center")
+
+        // Cria a checkbox
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = tarefa.status === 'c'; // Se a tarefa estiver concluída, marca a checkbox
+        checkbox.classList.add("checkbox", "checkbox-info", "mr-2");
+
+
+        // Define o comportamento da checkbox
+        checkbox.addEventListener("change", () => {
+            tarefa.status = checkbox.checked ? 'c' : 'a';
+
+            // Atualiza o localStorage
+            tarefas[index] = tarefa;
+            localStorage.setItem("tarefas", JSON.stringify(tarefas));
+
+            carregarTarefas();
+        });
+
+        // Cria um texto para a tarefa
+        const textoTarefa = document.createElement("span");
+        textoTarefa.classList.add("max-w-[180px]", "min-[370px]:max-w-[230px]",
+        "md:max-w-[310px]","break-words");
+        textoTarefa.textContent = tarefa.descricao;
+
+        if (tarefa.status === 'c') {
+            textoTarefa.classList.add("line-through"); 
+        }
+
+        const botao_excluir = document.createElement('button');
+        botao_excluir.dataset.index = index; // Define o índice no botão
+
+        botao_excluir.addEventListener("click", (event) => {
+            const indexClicado = event.currentTarget.dataset.index; // Obtém o índice do botão clicado
+            tarefas.splice(indexClicado, 1); // Remove a tarefa da lista
+            localStorage.setItem("tarefas", JSON.stringify(tarefas)); // Atualiza o localStorage
+            carregarTarefas();
+        });
+
+        const img = document.createElement('img');
+        img.src = "./src/img/trash.png";
+        img.alt = "excluir tarefa"
+        botao_excluir.appendChild(img);
+
+        // Adiciona a checkbox e o texto da tarefa à `li`
+        container.appendChild(checkbox);
+        container.appendChild(textoTarefa);
+
+        li.appendChild(container);
+        li.appendChild(botao_excluir);
+
+        lista.appendChild(li);
+    });
+}
+
+
 
